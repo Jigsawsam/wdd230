ae653544f444af9d00bb51a54a0079da6e7db9a1
Jigsawsam <bgt117@gmail.com>
2024-09-29 09:03:20 -0700

    chamber

---
+++ chamber/scripts/dates.js
@@ -0,0 +1,7 @@
+// Current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
